f81e0185f781c4aa35786a12f2c06f6a25174384
WiredOverload <mike.everywhere@gmail.com>
2024-02-23 22:25:45 -0600

    color picker

--- scenes/gameplay.gd
+++ scenes/gameplay.gd
@@ -3,12 +3,14 @@ extends Node2D
 var squareScene = preload("res://UI/pixel_square.tscn")
 @onready var grid = $GridContainer
 
+var selectedColor = Color.RED
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var square: ColorRect
 	for i in range(15*15):
 		square = squareScene.instantiate()
-		square.mouse_entered.connect(change_color.bind(square))
+		square.mouse_entered.connect(draw_color.bind(square))
 		grid.add_child(square)
 
 
@@ -17,7 +19,20 @@ func _process(delta: float) -> void:
 	pass
 
 
-func change_color(square: ColorRect) -> void:
+func draw_color(square: ColorRect) -> void:
 	if Input.is_mouse_button_pressed(1):
-		square.color = Color.RED
+		square.color = selectedColor
 
+
+
+func _on_red_button_pressed() -> void:
+	selectedColor = Color.RED
+
+func _on_blue_button_pressed() -> void:
+	selectedColor = Color.BLUE
+
+func _on_green_button_pressed() -> void:
+	selectedColor = Color.GREEN
+
+func _on_black_button_pressed() -> void:
+	selectedColor = Color.BLACK

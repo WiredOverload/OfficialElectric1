519fa82a45e563699f0d237227a1966d5865ad5f
WiredOverload <mike.everywhere@gmail.com>
2024-02-25 15:34:15 -0600

    rainbow rebalance

--- requests/rainbow.gd
+++ requests/rainbow.gd
@@ -10,16 +10,18 @@ func grade(img: Image) -> float:
 		Palette.BROWN: 0,
 		Palette.GREEN: 0,
 		Palette.MAGENTA: 0,
-		Palette.BLACK: 0,
-		Palette.WHITE: 0,
+		#Palette.BLACK: 0,
+		#Palette.WHITE: 0,
 		Palette.YELLOW: 0,
 	}
 	
 	var s := img.get_size()
 	var count := 0
+	var pixel
 	for y in s.y:
 		for x in s.x:
-			if img.get_pixel(x, y) != Color(0, 0, 0, 0):
+			pixel = img.get_pixel(x, y)
+			if pixel != Color(0, 0, 0, 0) && pixel != Palette.BLACK && pixel != Palette.WHITE:
 				color_counts[img.get_pixel(x, y)] += 1
 	return 1 if color_counts.values().all(func(x): x > 24) else -1
 

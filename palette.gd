666408acb303d7e763490407b1b16d8898c7de08
Apples <2352020+apples@users.noreply.github.com>
2024-02-25 13:38:51 -0600

    Tools

--- palette.gd
+++ palette.gd
@@ -1,14 +1,18 @@
 class_name Palette
 extends RefCounted
 
-const RED = Color(0.8936, 0.2295, 0.267, 1)
-const GREEN = Color(0.3561, 0.6718, 0.2291, 1)
-const BLUE = Color(0.4, 0.5389, 0.9633, 1)
-const CYAN = Color(0.125, 0.7021, 0.7082, 1)
-const MAGENTA = Color(0.8547, 0.2761, 0.8821, 1)
-const YELLOW = Color(0.949, 0.6706, 0.2157, 1)
-const BLACK = Color(0.0808, 0.0808, 0.0808, 1)
-const WHITE = Color(0.9254, 0.9254, 0.9254, 1)
+static var RED := Color(0.8936, 0.2295, 0.267, 1)
+static var GREEN := Color(0.3561, 0.6718, 0.2291, 1)
+static var BLUE := Color(0.4, 0.5389, 0.9633, 1)
+static var CYAN := Color(0.125, 0.7021, 0.7082, 1)
+static var MAGENTA := Color(0.8547, 0.2761, 0.8821, 1)
+static var YELLOW := Color(0.949, 0.6706, 0.2157, 1)
+static var BLACK := Color(0.0808, 0.0808, 0.0808, 1)
+static var WHITE := Color(0.9254, 0.9254, 0.9254, 1)
+
+static func _static_init() -> void:
+	for name: StringName in [&"RED",&"GREEN",&"BLUE",&"CYAN",&"MAGENTA",&"YELLOW",&"BLACK",&"WHITE"]:
+		Palette[name] = Color.hex(Palette[name].to_rgba32())
 
 static func color_name(c: Color) -> StringName:
 	for name: StringName in [&"RED",&"GREEN",&"BLUE",&"CYAN",&"MAGENTA",&"YELLOW",&"BLACK",&"WHITE"]:
--- scenes/gameplay.gd
+++ scenes/gameplay.gd
@@ -3,6 +3,11 @@ extends Node2D
 const TEXT_SPEED = 1.0
 const IMAGE_SIZE = 24
 
+enum Tool {
+	PENCIL,
+	BUCKET,
+}
+
 var requests: Array[Script] = [
 	preload("res://requests/more_red.gd"),
 	preload("res://requests/more_green.gd"),
@@ -11,8 +16,8 @@ var requests: Array[Script] = [
 ]
 
 var squareScene = preload("res://UI/pixel_square.tscn")
-@onready var grid = %PixelGrid
 @onready var request_label: RichTextLabel = %RequestLabel
+@onready var canvas: TextureRect = %Canvas
 
 @onready var palette_rects := {
 	RED = %PaletteRed,
@@ -25,8 +30,17 @@ var squareScene = preload("res://UI/pixel_square.tscn")
 	BLACK = %PaletteBlack,
 }
 
+@onready var tool_buttons := {
+	PENCIL = %PencilToolButton,
+	BUCKET = %BucketToolButton,
+}
+
 var selected_color := Palette.RED: set = set_selected_color
 var old_color = null
+
+var current_tool: Tool = Tool.PENCIL: set = set_current_tool
+
+var current_image: Image
 
 var current_subject := ""
 var current_request
@@ -53,18 +67,15 @@ var starting_phrases := [
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	selected_color = selected_color
+	current_tool = current_tool
 	
 	for c in palette_rects:
 		palette_rects[c].color = Palette[c]
 		palette_rects[c].gui_input.connect(_on_palette_gui_input.bind(palette_rects[c]))
 	
-	grid.columns = IMAGE_SIZE
-	for i in range(IMAGE_SIZE*IMAGE_SIZE):
-		var square: ColorRect = squareScene.instantiate()
-		square.mouse_entered.connect(draw_color.bind(null, square))
-		square.gui_input.connect(draw_color.bind(square))
-		square.mouse_exited.connect(exit_square.bind(square))
-		grid.add_child(square)
+	current_image = Image.create(IMAGE_SIZE, IMAGE_SIZE, false, Image.FORMAT_RGBA8)
+	(canvas.texture as ImageTexture).set_image(current_image)
+	canvas.gui_input.connect(_on_canvas_gui_input)
 	
 	#start_request()
 
@@ -90,10 +101,7 @@ func exit_square(square: ColorRect) -> void:
 		old_color = null
 
 func get_image() -> Image:
-	var image := Image.create(IMAGE_SIZE, IMAGE_SIZE, false, Image.FORMAT_RGB8)
-	for i in range(IMAGE_SIZE*IMAGE_SIZE):
-		image.set_pixel(i % IMAGE_SIZE, i / IMAGE_SIZE, grid.get_child(i).color)
-	return image
+	return current_image
 
 func start_request() -> void:
 	var script: Script = requests.pick_random()
@@ -136,6 +144,11 @@ func set_selected_color(c: Color) -> void:
 	selected_color = c
 	palette_rects[Palette.color_name(selected_color)].get_parent().theme_type_variation = &"PanelBorderOn"
 
+func set_current_tool(t: Tool) -> void:
+	tool_buttons[Tool.find_key(current_tool)].get_parent().theme_type_variation = &"PanelBorderOff"
+	current_tool = t
+	tool_buttons[Tool.find_key(current_tool)].get_parent().theme_type_variation = &"PanelBorderOn"
+
 func _on_palette_gui_input(event: InputEvent, palette_rect: ColorRect) -> void:
 	if event is InputEventMouseButton:
 		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
@@ -146,3 +159,61 @@ func _on_submit_button_pressed() -> void:
 		get_tree().reload_current_scene()
 	else:
 		next_request()
+
+func _canvas_to_pixel(canvas_coord: Vector2) -> Vector2i:
+	return Vector2i(canvas_coord / canvas.size * Vector2(IMAGE_SIZE, IMAGE_SIZE))
+
+func _on_canvas_gui_input(event: InputEvent) -> void:
+	match current_tool:
+		Tool.PENCIL:
+			if event is InputEventMouseButton:
+				if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+					var where := _canvas_to_pixel(event.position)
+					current_image.set_pixelv(where, selected_color)
+					_update_canvas_image()
+			
+			if event is InputEventMouseMotion:
+				if event.button_mask & MOUSE_BUTTON_MASK_LEFT:
+					var from := Vector2(_canvas_to_pixel(event.position - event.relative))
+					var to := Vector2(_canvas_to_pixel(event.position))
+					var n := maxi(absi(to.x - from.x), absi(to.y - from.y))
+					for i: int in n + 1:
+						var where := Vector2i(from.lerp(to, float(i) / float(n)).round())
+						if Rect2i(0, 0, IMAGE_SIZE, IMAGE_SIZE).has_point(where):
+							current_image.set_pixelv(where, selected_color)
+					_update_canvas_image()
+		
+		Tool.BUCKET:
+			if event is InputEventMouseButton:
+				if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+					var where := _canvas_to_pixel(event.position)
+					var inside := current_image.get_pixelv(where)
+					if inside == selected_color:
+						return
+					var stack := [where]
+					while not stack.is_empty():
+						var p: Vector2i = stack.pop_back()
+						if current_image.get_pixelv(p) != inside:
+							continue
+						current_image.set_pixelv(p, selected_color)
+						for d: Vector2i in [Vector2i.LEFT, Vector2i.RIGHT, Vector2i.UP, Vector2i.DOWN]:
+							var pp := p + d
+							if Rect2i(0, 0, IMAGE_SIZE, IMAGE_SIZE).has_point(pp):
+								stack.append(pp)
+					_update_canvas_image()
+
+
+func _update_canvas_image() -> void:
+	(canvas.texture as ImageTexture).update(current_image)
+
+
+func _on_pencil_tool_button_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+			current_tool = Tool.PENCIL
+
+
+func _on_bucket_tool_button_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+			current_tool = Tool.BUCKET
